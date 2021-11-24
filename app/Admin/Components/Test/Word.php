461e6d4ae786ab8648b3ab4a0dee18b2960724ae
翔翔 <515007542@qq.com>
2021-11-24 14:22:33 +0800

    Update Word.php

--- app/Admin/Components/Test/Word.php
+++ app/Admin/Components/Test/Word.php
@@ -17,9 +17,9 @@ class Word extends Base
 
 
         $this->hidden('word_compound')->default(1);
-        $this->text('lon_lat', '插入图片放置坐标')->help('例 100, 100  坐标将左右偏差部分');
+        $this->text('lon_lat', '插入图片放置坐标')->help('例 100, 100  坐标将左右偏差部分, 该坐标为在ps中要放置插入图片的坐标');
         $this->file('image_template', '插入图片');
-        $this->multipleFile('words', '模板图片');
+        $this->multipleFile('words', '模板图片')->help('模板图片为统一样例图片, 另坐标位置不可在任何模板图片之外');
 
 
     }
